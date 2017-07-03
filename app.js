12bc13b931261f9bbdbe092de51ea979f08a5ae6
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 11:56:01 +0200

    testing map iterators

--- app.js
+++ app.js
@@ -8,3 +8,16 @@ console.log(myMap.get('qwerty'));
 console.log(myMap.size);
 console.log(myMap.has('Hello'));
 console.log(myMap.has('hello'));
+
+
+for(let key of myMap.keys()) {
+  console.log(key);
+}
+
+for(let value of myMap.values()) {
+  console.log(value);
+}
+
+for(let [key, value] of myMap.entries()) {
+  console.log(key + " " + value);
+}
