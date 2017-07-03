c51a7dc0ba87d849d5a5799dff3176d68c922f9c
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 10:24:29 +0200

    salut

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-let first = [1,2,3];
-let second = [4,5,6];
+let salutation = "hi";
+let greeting = salutation + ' Patryk';
 
-function addThreeThings(a, b, c) {
-  let result = a + b + c;
-  console.log(result);
-}
-
-addThreeThings(...first);
+console.log(greeting);
