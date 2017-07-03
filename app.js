c2ed39c1af7a3dc05b13c7c672e19036b164c438
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 10:19:37 +0200

    spread array

--- app.js
+++ app.js
@@ -1,14 +1,1 @@
-let color = "Czerwony";
-let speed = 10;
-
-let car = {
-  color,
-  speed,
-  go() {
-    console.log('Brrrumm');
-  }
-};
-
-console.log(car.color);
-console.log(car.speed);
-car.go();
+console.log(...[1,2,3])
