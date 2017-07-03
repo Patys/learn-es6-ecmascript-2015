f91059a51a267b569a6b85a3d2696dfa204e9320
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 10:38:37 +0200

    destructed object

--- app.js
+++ app.js
@@ -1,13 +1,5 @@
-function checkTime(strings, ...values) {
-  if(values[0] < 22) {
-    values[1] = "awake";
-  } else {
-    values[1] = "sleepy";
-  }
-
-  return `${strings[0]}${values[0]}${strings[1]}${values[1]}`
+let {color} = {
+  color: "red"
 }
 
-let message = checkTime`It's ${new Date().getHours()} and I'm ${""}`;
-
-console.log(message);
+console.log(color);
