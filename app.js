1066082c52b24d9ed69d0f13de221d98cfe97283
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 10:35:23 +0200

    string func template string

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 function checkTime(strings, ...values) {
-  console.log(strings);
-  console.log(values);
+  if(values[0] < 22) {
+    values[1] = "awake";
+  } else {
+    values[1] = "sleepy";
+  }
+
+  return `${strings[0]}${values[0]}${strings[1]}${values[1]}`
 }
 
-let message = checkTime`It's ${new Date().getHours()} and I'm sleepy`;
+let message = checkTime`It's ${new Date().getHours()} and I'm ${""}`;
 
 console.log(message);
