2cceb8e302bb1f3661a3735d7d45dc2026ba1ca6
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 12:01:46 +0200

    using arguments

--- app.js
+++ app.js
@@ -1,28 +1,5 @@
-let myMap = new Map();
-
-let myObj = {}
-let myFunc = function(){}
-
-myMap.set('foo', 'bar');
-myMap.set('Hello', 'World');
-myMap.set(myObj, 'obj');
-myMap.set(myFunc, 'func');
-
-console.log(myMap.get('foo'));
-console.log(myMap.get('qwerty'));
-console.log(myMap.size);
-console.log(myMap.has('Hello'));
-console.log(myMap.has('hello'));
-
-
-for(let key of myMap.keys()) {
-  console.log(key);
+function myFunc() {
+  console.log(arguments);
 }
 
-for(let value of myMap.values()) {
-  console.log(value);
-}
-
-for(let [key, value] of myMap.entries()) {
-  console.log(key + " " + value);
-}
+myFunc(1,2,3);
