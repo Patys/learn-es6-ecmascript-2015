7e8088e2853ab9d598aea02e3580ab575c59c887
Patryk Szczygło <patysprog@gmail.com>
2017-07-03 11:25:22 +0200

    added lodash (where is deprecated)

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+import * as _ from 'lodash';
+
 import {sumTwo, sumThree} from './math/addition';
+import {users} from './data/users';
+
+console.log(_.filter(users, {age:20}));
 
 console.log('2 + 3 = ', sumTwo(2,3));
 console.log('2 + 3 + 4 = ', sumTwo(2,3,4));
